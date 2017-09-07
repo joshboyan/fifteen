7dc4b5c325ef45a19a64a4e4723c2fd27bd0388a
joshboyan <joshboyan@yahoo.com>
2017-09-06 21:38:27 -0700

    heroku deploy

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  port: process.env.PORT || 5000,
+  dbURI: 'mongodb://josh11:josh11@ds115214.mlab.com:15214/heroku_nfl4r94m',
+  user: 'boyanjosh@gmail.com',
+  pass: '24045Hss!'
+}
